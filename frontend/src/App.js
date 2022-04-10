745cd4a7b938ff8d79fab5606f75989d528d892f
chacheng21 <charles201564cheng@gmail.com>
2022-04-10 18:20:15 -0400

    add photo to s3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,44 @@
-export function App() {
-  return <h1>Hello world!</h1>;
+import React, { useState } from 'react'
+import AWS from 'aws-sdk'
+import crypto from 'crypto'
+import { promisify } from 'util'
+import securityCredentials from '../../aws_info'
+
+const randomBytes = promisify(crypto.randomBytes)
+
+const App = () => {
+  const upload = async e => {
+    AWS.config.update({
+      accessKeyId: securityCredentials.accessKeyId,
+      secretAccessKey: securityCredentials.secretAccessKey,
+      region: 'us-east-1',
+    })
+
+    const rawBytes = await randomBytes(16)
+    const imageName = rawBytes.toString('hex')
+
+    // IMAGE LINK (img src): https://cis-197-images.s3.amazonaws.com/${imageName}
+
+    const s3 = new AWS.S3()
+    await s3.putObject({
+      Body: e.target.files[0],
+      Bucket: 'cis-197-images',
+      Key: imageName,
+    })
+  }
+
+  const submitForm = () => {
+
+  }
+
+  return (
+    <div className="App">
+      <form>
+        <input type="file" onChange={e => upload(e)} />
+        <button type="submit" onClick={() => submitForm()}>Submit</button>
+      </form>
+    </div>
+  )
 }
+
+export default App
